847c2d79d20c84dcdea9d245d539ba5d078ba1dd
unknown <ejjigiri.chandu@gmail.com>
2022-04-28 07:54:48 +0530

    commit

---
+++ ForLoopExample.java
@@ -0,0 +1,27 @@
+
+
+    public class ForLoopExample {
+        public static void main(String[] args) {
+
+
+            for (int i = 0; i <= 10; i++) {
+                System.out.println(" i ===");
+            }
+
+            int i=0;
+            while(i<=10){
+                System.out.println("i = " + i);
+                i++;
+            }
+            System.out.println(i);
+
+
+            int i1=12;
+            do{
+                System.out.println("i1 = " + i1);
+                i1++;
+            }while(i1<=10);
+
+        }
+    }
+
---
+++ InheritanceExample.java
@@ -0,0 +1,53 @@
+
+
+class C{
+    int a;
+    void show(){
+        System.out.println("from show function");
+    }
+    public void add(int a, int b){
+        System.out.println("form add = "+ (a+b));
+    }
+
+}
+public class InheritanceExample extends C {
+
+    public void disaplay(){
+        System.out.println("from mdisaply");
+    }
+    public void add(int a , int b){
+        System.out.println("form add from sub  class = "+(a+b));
+    }
+    void show2(){
+        System.out.println("from show2");
+    }
+    public static void main(String[] args) {
+
+        System.out.println(args[0]+"  "+args[1]);
+        C obj = new C();
+        obj.add(3,4);
+
+        obj.show();// this function calling from same class no inheritance
+
+        InheritanceExample obj1 = new InheritanceExample();
+        obj1.show(); // this function calling from super class this is inheritance
+        obj1.add(3,6);
+        obj1.show2();
+
+        C obj3 = new InheritanceExample();
+        obj3.add(7,8);  //sub class
+
+        obj3.show();
+
+    }
+}
+class D extends  InheritanceExample{
+
+    public static void main(String[] args) {
+        D obj = new D();
+        obj.show();
+        obj.disaplay();
+    }
+
+}
+
